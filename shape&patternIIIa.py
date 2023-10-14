9273a66136c89602ebd05a7499777849ea778a81
Darwinian_Living_Machine2000 <118555211+Sila2000@users.noreply.github.com>
2023-10-14 11:45:43 +0530

    Add files via upload

---
+++ shape&patternIIIa.py
@@ -0,0 +1,10 @@
+row = int(input("Enter the number of rows: "))
+character = input("Enter the character: ")
+
+#a damn parallelogram
+for i in range(row, 0, -1):
+    for j in range(i):
+        print(" ", end=' ')
+    for j in range(row):
+        print(character, end=' ')
+    print("\r")
